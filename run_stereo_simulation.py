9e2ea147c24ff74659ea2d862b7d0c014187ebe5
divadnauj-GB <juan.guerrero@polito.it>
2024-01-28 15:41:18 +0000

    updated repo

---
+++ run_stereo_simulation.py
@@ -0,0 +1,35 @@
+import os
+import filecmp
+import subprocess
+
+import numpy as np 
+from PIL import Image
+
+import Image_input_test
+import Image_result_test
+
+Left_image = np.array(Image.open("./im2.png").convert('L'))
+Right_image = np.array(Image.open("./im6.png").convert('L'))
+
+im_shape=Left_image.shape
+
+im_shape=Left_image.shape
+if len(im_shape)<3:
+    (N_filas, N_columnas)=im_shape
+    num_channels = 0
+else:
+    (N_filas, N_columnas, num_channels)=im_shape
+    
+print(im_shape)
+
+Image_input_test.serialize_stereo_images(Left_image=Left_image,Right_image=Right_image)
+
+command = "vsim -c -do vsim_compile.tcl"
+print(command)
+os.system(command)
+
+Image_result_test.create_disparity_map(N_filas,N_columnas,3)
+
+# removing parameters from previous configurations:
+File_path=os.getcwd()
+print(File_path)
